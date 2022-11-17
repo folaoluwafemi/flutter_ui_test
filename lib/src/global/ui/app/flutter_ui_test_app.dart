b763fdd963cb1f9fc1326e8fa8546c30dfb42e85
folaoluwfemi <folaoluwafemi55@gmail.com>
2022-11-17 16:28:14 +0100

    complete implementation

--- lib/src/global/ui/app/flutter_ui_test_app.dart
+++ lib/src/global/ui/app/flutter_ui_test_app.dart
@@ -8,6 +8,7 @@ class FlutterUiTestApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const SettingsScreen(),
       themeMode: ThemeMode.light,
       theme: AppThemes.lightTheme,
